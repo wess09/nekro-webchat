e2676b251ae6f42a776ec43ea2f18c97f3668bc5
wess09 <zicheng_1202@outlook.com>
2026-04-26 23:31:17 +0800

    123

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  publicDir: 'static',
   plugins: [react()],
   server: {
     host: true,
@@ -32,4 +32,3 @@ export default defineConfig({
     }
   }
 })
-
